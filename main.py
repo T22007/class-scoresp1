a80ef92aa2deb3da86fd096b7b312332978d64f5
T22 <78694408+T22007@users.noreply.github.com>
2023-09-14 13:23:24 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import simplenomreh
+def main():
+    nomreh=school("my school")
+while 1:
+    choice=int(input("1:add\n2:sum\n3:averge\n4:min\n:5;max\n;6:exit"))
+    if choice==1:
+        simplenomreh.add()
+    elif choice==2:
+        simplenomreh.sum
+    elif choice==3:
+        simplenomreh.averge
+    elif choice==4:
+        simplenomreh.min
+    elif choice==5:
+        simplenomreh.max
+    elif choice==6:
+        simplenomreh.exit
+    elif choice==7:
+        break
+
+
+main()
